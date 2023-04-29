ca5ae6848b1d1afe16839a7df83cde6f0a14d967
Vinícius <107147814+vini790@users.noreply.github.com>
2023-04-29 18:18:22 -0300

    Create Calculating_CPF.py
    predecessor results:
    '''CPF, digit1, digit2 = [4, 5, 0, 2, 0, 3, 7, 1, 8], 4, 0
    calculation_numbers = range(10, 1, -1)
    sum = 0
    for n in range(len(CPF)):
        calculation = CPF[n] * numbers_calculations[n]
        sum += calculation
    sum *= 10
    valid_digit_1 = sum % 11
    print("VALID DIGIT1" if valid_digit_1 == digit1 else "INVALID DIGIT1")
    CPF.append(digit1)
    calculation_numbers = range(11, 1, -1)
    sum = 0
    for n in range(len(CPF)):
        calculation = CPF[n] * numbers_calculations[n]
        sum += calculation
    sum *= 11
    valid_digit_2 = sum % 11
    print("VALID DIGIT2" if valid_digit_2 == digit2 else "INVALID DIGIT2")
    CPF.append(digit2)
    print(CPF)
    
    def calculation_cpf(CPF):
    
        numbers_calculations_digit1 = range(10, 1, -1)
        cpf_numbers = []
        sum_digit1 = 0
    
        if len(CPF) == 11:
    
            for i, num in enumerate(CPF[0]):
                cpf_numbers.append(int(num))
            
            for cont in range(len(cpf_numbers)):
    
                if count >= 9:
                    continues
    
                calculation_digit1 = numbers_cpf[count] * numbers_calculations_digit1[count]
                sum_digit1 += calculate_digit1
            digit1_valido = (sum_digit1 * 10) % 11
    
            if digito1_valido == numeric_cpf[9]:
    
                print("VALID DIGIT1")
                numbers_calculations_digit2 = range(11, 1, -1)
                sum_digit2 = 0
    
                for cont in range(len(cpf_numbers)):
    
                    if count >= 10:
                        continues
    
                    calculo_digito2 = numerals_cpf[count] * numerals_calculos_digito2[count]
                    sum_digit2 += calculate_digit2
                digit2_valido = (sum_digit2 * 11) % 11
    
                if digito2_valido == numeric_cpf[10]:
                    print("VALID DIGIT2")
                    return true
    
                else:
                    print("INVALID DIGIT2")
                    return False
    
            else:
                print("INVALID TYPE 1")
                return False
        else:
            print("ENTER THE NUMBERS CORRECTLY")
    
    
    cpf = ['74682489070']
    cpf_calculus(cpf)'''
    # TO IMPROVE

---
+++ Calculating_CPF.py
@@ -0,0 +1,46 @@
+# use of the program for student purposes only
+
+def calculate_cpf(CPF):
+ 
+    if len(CPF) == 11:
+ 
+        try:
+ 
+            sum_digit1 = 0
+            cpf_numbers = CPF[:9]
+            numbers_calculation_digit = range(10, 1, -1)
+            
+            for cont in range(len(cpf_numbers)):
+                digit_calculation1 = int(cpf_numbers[count]) * digit_calculation_numbers[count]
+                sum_digit1 += calculate_digit1
+            digit1_valido = (sum_digit1 * 10) % 11
+ 
+            if digit1_valido > 9:
+                digit1_valido = 0
+ 
+            sum_digit2 = 0
+            cpf_numbers = CPF[:10]
+            numbers_calculation_digit = range(11, 1, -1)
+ 
+            for cont in range(len(cpf_numbers)):
+                digit_calculation2 = int(cpf_numbers[count]) * digit_calculation_numbers[count]
+                sum_digit2 += calculate_digit2
+            digit2_valido = (sum_digit2 * 11) % 11
+ 
+            if digito2_valido > 9:
+                digit2_valido = 0
+ 
+            if digito1_valido == int(CPF[9]) and digito2_valido == int(CPF[10]):
+                return true
+            else:
+                return False
+ 
+        except ValueError:
+            return False
+ 
+ 
+cpf = '74682489070'
+if calculate_cpf(cpf):
+    print("VALID CPF")
+else:
+    print("INVALID CPF")
