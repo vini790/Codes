ceb87785c136e78406d86d13f0b47a5ebb45910a
Vinícius <107147814+vini790@users.noreply.github.com>
2023-09-20 17:30:16 -0300

    Update_Calculating_CPF.py

--- Calculating_CPF.py
+++ Calculating_CPF.py
@@ -1,46 +1,75 @@
-# use of the program for student purposes only
+class CPF:
+    '''Clase para validação de CPF'''
 
-def calculate_cpf(CPF):
- 
-    if len(CPF) == 11:
- 
-        try:
- 
-            sum_digit1 = 0
-            cpf_numbers = CPF[:9]
-            numbers_calculation_digit = range(10, 1, -1)
-            
-            for cont in range(len(cpf_numbers)):
-                digit_calculation1 = int(cpf_numbers[count]) * digit_calculation_numbers[count]
-                sum_digit1 += calculate_digit1
-            digit1_valido = (sum_digit1 * 10) % 11
- 
-            if digit1_valido > 9:
-                digit1_valido = 0
- 
-            sum_digit2 = 0
-            cpf_numbers = CPF[:10]
-            numbers_calculation_digit = range(11, 1, -1)
- 
-            for cont in range(len(cpf_numbers)):
-                digit_calculation2 = int(cpf_numbers[count]) * digit_calculation_numbers[count]
-                sum_digit2 += calculate_digit2
-            digit2_valido = (sum_digit2 * 10) % 11
- 
-            if digito2_valido > 9:
-                digit2_valido = 0
- 
-            if digito1_valido == int(CPF[9]) and digito2_valido == int(CPF[10]):
-                return True
-            else:
-                return False
- 
-        except ValueError:
-            return False
- 
- 
-cpf = '74682489070'
-if calculate_cpf(cpf):
-    print("VALID CPF")
+    def __init__(self, cpf='00000000000'):
+        """
+        Inicializa uma instância da classe CPF.
+
+        Args:
+            cpf (str): O CPF a ser validado, representado como uma string de 11 dígitos numéricos.
+        """
+
+        self._cpf = cpf
+        self._sum1 = list(range(10, 1, -1))
+        self._sum2 = list(range(11, 1, -1))
+        self._cpf_valido = self._valida_cpf()
+
+    def _primeiro_digito(self):
+        """
+        Calcula o primeiro dígito verificador do CPF.
+
+        Returns:
+            int: O primeiro dígito verificador.
+        """
+
+        dig1 = sum([(int(n) * s) for n, s in zip(self._cpf, self._sum1)]) % 11
+        return 0 if dig1 < 2 else 11 - dig1
+
+    def _segundo_digito(self):
+        """
+        Calcula o segundo dígito verificador do CPF.
+
+        Returns:
+            int: O primeiro dígito verificador.
+        """
+        dig2 = sum([(int(n) * s) for n, s in zip(self._cpf, self._sum2)]) % 11
+        return 0 if dig2 < 2 else 11 - dig2
+
+    def _valida_cpf(self):
+        """
+        Valida se o CPF fornecido é válido.
+
+        Returns:
+            bool: True se o CPF é válido, False caso contrário.
+        
+        Raises:
+            ValueError: Se o CPF não possuir 11 dígitos numéricos.
+        """
+
+        if len(self._cpf) == 11 and self._cpf.isnumeric():
+            digitos1 = list(self._cpf[-2:])
+            dig1 = self._primeiro_digito()
+            dig2 = self._segundo_digito()
+            digitos2 = [str(dig1), str(dig2)]
+
+            return digitos2 == digitos1
+
+        raise ValueError('Seu CPF deve conter 11 digitos')
+
+    def cpf_valido(self):
+        """
+        Verifica se o CPF fornecido é válido.
+
+        Returns:
+            bool: True se o CPF é válido, False caso contrário.
+        """
+        
+        return self._cpf_valido
+
+
+#Exemplo de uso:
+'''cpf = CPF('11122233344')  # Digite o CPF aqui
+if cpf.cpf_valido():
+    print("CPF válido")
 else:
-    print("INVALID CPF")
+    print("CPF inválido")'''
